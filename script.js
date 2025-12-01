f15a4c012b676d9ebc8f1258c23c0bb31f07ef78
alessiaimprota <alessiaimprota126@gmail.com>
2025-12-01 16:14:47 +0100

    real price of the ticket

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-//Chiedere il kilometraggio da percorrere in treno all'utente (userKm)
-// Moltiplicare il kilometraggio per la tariffa fissa per avere il prezzo pieno del biglietto
-// priceTicket:(userKm * 0.21 Euro)
-//Scontistiche
-// SE l'user ha meno di 18 anni (priceTicket -20%)
-// ALTRIMENTI SE  l'user ha più di 60 anni (priceTicket-40%)
-//ALTRIMENTI il prezzo resta invariato
+/*Chiedere il kilometraggio da percorrere in treno all'utente (userKm)
+ Moltiplicare il kilometraggio per la tariffa fissa per avere il prezzo pieno del biglietto
+priceTicket:(userKm * 0.21 Euro)
+ Scontistiche
+SE l'user ha meno di 18 anni (priceTicket -20%)
+ userAge<=18 
+ALTRIMENTI SE  l'user ha più di 60 anni (priceTicket-40%)
+ userAge=> 65
+ALTRIMENTI il prezzo resta invariato
+*/
 
 //Setup
 const priceKm = 0.21;
@@ -23,6 +26,9 @@ console.log(userKm, typeof userKm);
 //calcolo in base all'età il prezzo pieno del bieglietto
 
 const priceTicket = userKm * priceKm;
-console.log(priceTicket);
-
+console.log(
+`
+ll prezzo del biglietto è ${priceTicket} \u20AC
+`    
+);
 
